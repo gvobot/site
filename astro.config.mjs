23ba6e017e32cc20dbeb18043e0da60cf6f4d9c6
Ducko <micasniebuhr@gmail.com>
2023-08-08 11:41:35 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://gvobot.app/",
+  redirects: {
+    "/invite": "https://discord.com/api/oauth2/authorize?client_id=1127781120000397402&permissions=10432408972535&scope=bot%20applications.commands",
+    "/discord": "https://discord.gg/ryWtHGVUte",
+    "/blog": "https://blog.gvobot.app",
+    "/github": "https://github.com/gvobot",
+    "/tiktok": "https://www.tiktok.com/@gvobot",
+    "/kofi": "https://ko-fi.com/duckodas",
+    "/patreon": "https://patreon.com/DuckoDas",
+  },
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
